14c168196c820973a2f9ba53c9e674a3745343bb
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-03-10 18:52:53 +0530

    infinite memory allocation 1. Added Program to demonstrate how memory is allocated infinitely.

--- DS/infi.c
+++ DS/infi.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+void create(int*);
 int main()
 {
-	printf("Hello\n");
-	main();//Causes infinite loop of calling recursive function.
+	int *n;
+	int ch;
+	printf("Say your command i will allocate memory:");
+	scanf("%d",&ch);
+	if(ch==1)
+		while (1)
+		{
+			create(n);
+		}
+		
 	return 0;
+}
+
+void create(int *n)
+{
+	n=(int*) malloc(1*sizeof(int));
 }

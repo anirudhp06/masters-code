74460b1b81235eaa103c1b9beae8d07a38a72e6c
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-03-13 18:45:16 +0530

    Added gcd_lcm.c code. minor changes to other files.

--- DS/stack.c
+++ DS/stack.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 5
+#define MAX 3
 int TOP=-1;
 
 void PUSH(int []);
--- DS/struct.c
+++ DS/struct.c
@@ -2,19 +2,14 @@
 
 struct stud
 {
-    int usn;
+    int usn,m1,m2,m3;
     char name[20];
-    int m1,m2,m3;
 };
 
 int main()
 {
     typedef struct stud s;
     s s1;
-    printf("Enter name:");
-    scanf("%s",s1.name);
-    printf("Enter USN:");
-    scanf("%d",&s1.usn);
-    printf("Name:%s\nUSN:%d",s1.name,s1.usn);
+    
     return 0;
 }

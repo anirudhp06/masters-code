04dc426382ccccbd49039e15e2c50960836379a1
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-03-20 19:42:16 +0530

    added unix pgm4a

---
+++ UNIX/pgm4a.sh
@@ -0,0 +1,22 @@
+clear
+if [ $# -eq 0 ]
+then
+	echo "No arguments passed."
+	exit
+fi
+if [ -e file.txt ]
+then
+	rm file.txt
+fi
+for file in $*
+do
+	if [ -e $file ]
+	then
+		echo "$file is valid file"
+		`echo $file >> "file.txt"`
+	else
+		echo "$file is invalid file type"
+	fi
+done
+echo "The given file names in UPPERCASE are as below"
+tr "a-z" "A-Z" < file.txt

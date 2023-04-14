da4c830cf7051fc390e37595d3a98666e4845ff8
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-04-14 08:05:55 +0530

    bash fixes

--- UNIX/pgm1a.sh
+++ UNIX/pgm1a.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 #Program to take distance between 2 cities(in KM) as input and display it as centimeters,Feet,Inches,meters
 echo "Enter Distance between 2 Cities(in KM):"
 read dist
 mts=`expr $dist \* 1000`
 cm=`expr $mts \* 100`
 ft=`echo "$mts * 3.281" | bc`
-inches=`echo "$mts * 39.37"|bc`
+inches=`echo "$mts * 39.37"| bc`
 
 echo "$dist km in meters:$mts m."
 echo "$dist km in cm:$cm cm."

b68f562941e8a3fecada7fbcf4550de8b89c8266
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-04-28 22:42:33 +0530

    updated voter.sh

--- UNIX/assignment/voter.sh
+++ UNIX/assignment/voter.sh
@@ -1,6 +1,9 @@
 echo "Enter name of person:"
 read name
-case $name in
-	"Anirudh" | "Nagendra" | "Sunidhi" | "Anusha")echo "Eligible to vote.";;
-	*)echo "Not in voter list";;
+echo "Enter Age:"
+read age
+case $age in
+	1[8-9]|[2-9][0-9])echo "Eligible to vote.";;
+	[0-9]|1[0-7])echo "Not eligible to vote";;
+	*)echo "Invalid option";;
 esac

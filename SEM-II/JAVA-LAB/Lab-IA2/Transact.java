0fb517e38c9c677ed321cc03bc5a0549c0cf57bc
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-09-19 20:24:09 +0530

    java lab IA2

---
+++ SEM-II/JAVA-LAB/Lab-IA2/Transact.java
@@ -0,0 +1,44 @@
+class UserDefined extends Exception {
+    public UserDefined(float amt) {
+        System.out.println("Withdrawing Amount is invalid:" + amt);
+    }
+}
+
+class Account {
+    float bal;
+
+    public Account(float bal) {
+        if (bal < 500.00f) {
+            System.out.println("Insufficient Balance.");
+        } else {
+            this.bal = bal;
+        }
+    }
+
+    public void deposit(float bal) {
+        this.bal += bal;
+        System.out.println("The balance is:" + this.bal);
+    }
+
+    public void withdraw(float amt) throws UserDefined {
+        if (amt > bal) {
+            throw new UserDefined(amt);
+        } else {
+            bal -= amt;
+            System.out.println("The balance is:" + bal);
+        }
+    }
+}
+
+class Transact {
+    public static void main(String[] args) {
+        Account a = new Account(1000.00f);
+        a.deposit(500.00f);
+        try {
+            a.withdraw(200.00f);
+            a.withdraw(2000.00f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

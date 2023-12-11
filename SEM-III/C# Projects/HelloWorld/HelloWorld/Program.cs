bdd5ca60a640da2a0e6103f1c43bfc13cf35d03d
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-12-11 20:01:00 +0530

    First commit of 3rd sem. Added C-sharp first lab program project. more yet to come.

---
+++ SEM-III/C# Projects/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0} arg is {1}", i + 1, args[i]);
+            }
+            Console.Read();
+        }
+    }
+}

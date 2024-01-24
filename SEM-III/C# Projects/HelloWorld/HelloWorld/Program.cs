08a99bb6e2a674878fc17e7742177156e40f1d0c
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2024-01-24 18:43:31 +0530

    Lab IA programs

--- SEM-III/C# Projects/HelloWorld/HelloWorld/Program.cs
+++ SEM-III/C# Projects/HelloWorld/HelloWorld/Program.cs
@@ -5,29 +5,26 @@ using System.Text;
 //C# program to handle Exceptions 
 namespace HelloWorld
 {
+    abstract public class Maths
+    {
+        abstract public int operation(int a,int b);
+    }
+    class Multiplication : Maths
+    {
+        public override int operation(int a, int b)
+        {
+            return a*b;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int[] arr = new int[5] { 10, 20, 30, 40, 50 };
-            int sum = 0;
-            try
-            {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    sum += arr[i];
-                }
-                Console.WriteLine("Sum is {0}", sum);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Execution completed");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Abstract class program");
+            Maths m = new Multiplication();
+            int a = m.operation(20, 30);
+            Console.WriteLine("Multiplication of 20*30={0}",a);
+            Console.ReadLine();
         }
     }
 }
